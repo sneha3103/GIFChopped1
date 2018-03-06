069671201089b5903f59f829168c30935d179c01
Sneha Dama <snehadama@Snehas-Air.lan>
2018-03-06 16:30:21 -0600

    Added a few comments explaining my lines of code

--- GifChopped/assets/javascript/gif.js
+++ GifChopped/assets/javascript/gif.js
@@ -9,7 +9,9 @@ function displayFoodItemInfo () {
     var food = $(this).attr("data-name");
     var foodStr = food.split(" ").join("+");
     var APIKey =  "DjWtmM2fUL6AYn27DkHYvhO436OOAUck";
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + foodStr + "&api_key=" + APIkey + "&limit=5";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + foodStr + "&api_key=" + APIkey + "&limit=5";
+
+    //Need to create AJAX call 
 
 }
 
@@ -18,16 +20,17 @@ function createButton () {
     $("#buttons-food").empty();
 
     for (var i = 0; i < topics.length; i++) {
-
+        //Generating buttons for each movie in the array and giving it a class of food-btn.
         var a = $("<button>");
 
         a.addClass("food-btn");
+        a.attr("data-name", topics[i]);
         a.text(topics[i]);
         $("#buttons-food").append(a);
 
     };
 };
-
+   //When movie button is clicked, the following function triggers. Event prevent default stops page from reloading and erasing prior data. 
     $("#add-item").on("click", function(event) {
 
         event.preventDefault();
@@ -36,8 +39,10 @@ function createButton () {
 
         createButton ();
     });
-
+    //Attaching entire html document on the event handler on the class food-btn when user clicks. 
     $(document).on("click", ".food-btn", displayFoodItemInfo);
 
+
+    //Called this function to display the buttons
     createButton ();
 
